7c65b9fec398777ec64ded551c2bc0338610ffcf
Fariha <farihaorna@gmail.com>
2024-12-09 21:39:54 -0500

    added form commponent with basic structure and responsive style

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+const Form = () => {
+  return (
+    <div className="form-container">
+    <form>
+      <label>
+        <input placeholder="e.g onion" className="form-input"></input>
+      </label>
+    </form>
+    <div className="add-btn">
+    + Add ingredient
+    </div>
+    </div>
+
+  )
+}
+
+export default Form;
