7227b3f85d0c7169e831a0d6a2e6ac3f7fdbaa7d
Fariha <farihaorna@gmail.com>
2024-12-11 14:08:20 -0500

    Refactored my code by simplifying submission form logic

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,7 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: 'detect' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -28,11 +28,11 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // Disable prop-types validation
     },
   },
 ]
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,38 +2,30 @@ import { useState } from "react";
 import List from "./List";
 
 const Form = () => {
-
-  let [ingredient, setIngredient] = useState("");
   let [ingredientList, setIngredientList] = useState([])
 
-  let getIngredient = (e) => {
-    setIngredient(e.target.value)
-  }
-
-  let addIngredient = (e) => {
-    e. preventDefault()
-    if(ingredient.trim() != ""){
-      setIngredientList (prevList => [...prevList,ingredient])
-      setIngredient("")
+  const addItemHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const new_ingredient = formData.get("ingredient")
+    if (new_ingredient) {
+      setIngredientList((prevList) => [...prevList, new_ingredient]);
+      e.target.reset();
     }
   }
 
   return (
     <>
     <div className="form-container">
-    <form onSubmit={addIngredient}>
+    <form onSubmit={addItemHandler}>
       <label>
         <input placeholder="e.g onion" 
         className="form-input" 
         name="ingredient" 
-        value={ingredient}
-        onChange={getIngredient}></input>
+        ></input>
       </label>
+      <button className="add-btn" type="submit">+ Add ingredient</button>
     </form>
-    <div className="add-btn"
-    onClick={addIngredient}>
-    + Add ingredient
-    </div>
     </div>
     <List ingredientList = {ingredientList}/>
     </>
