21726bc01e4eb322cdb22b31e479cc98516b7019
Fariha <farihaorna@gmail.com>
2024-12-12 17:29:17 -0500

    added get recipe section with its style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,8 @@ const List = (props) => {
   return (
     <>
     {
-    props.ingredientList.length > 0 ? (
+    props.ingredientList.length > 0  && 
+    <section>
     <div className="ingredient-list-container">
     <span className="list-title">Ingredients on hand:</span>
     <ul>
@@ -10,7 +11,15 @@ const List = (props) => {
     return <li key={ingredient}>{ingredient}</li>
     })}
     </ul>
-    </div> ): ("")
+    </div> 
+    <div className="get-recipe-container">
+    <div className="get-recipe-text">
+      <span>Ready for a recipe?</span>
+      <p>Generate a recipe from your list of ingredients.</p>
+    </div>
+    <button className="generateBtn">Get a recipe</button>
+    </div>
+  </section>  
   }
   </>
   )
